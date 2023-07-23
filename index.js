56d9700a7b77c47b0d3f55b39236d2b173e53935
eugenehan94 <eugene.han94@gmail.com>
2023-07-23 12:27:36 -0400

    finished review of Node theory - first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Promise.resolve().then(() => console.log("this is promise.resolve 1"));
+// process.nextTick(()=>console.log("This is process.nextTick 1"));
+
+// console.log("console.log 1");
+// process.nextTick(() => console.log('this is process.next 1'));
+// console.log("console.log 2");
